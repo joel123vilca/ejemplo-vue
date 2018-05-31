68a3618ee13d7117c8c7929122afef696d281bc8
joel123vilca <joevil23tjq@gmail.com>
2018-05-30 21:30:41 -0500

    Crear componentes agregar tarea

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo :titulo="titulo"></titulo>
+      <nueva-tarea v-bind:tareas="tareas"></nueva-tarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import Titulo from './TituloComponent.vue'
+import NuevaTarea from './NuevaTareaComponent.vue'
+export default {
+  components: {
+    Titulo,
+    NuevaTarea
+  },
+  data(){
+    return{
+      titulo: 'Lista de tareas',
+      tareas:[
+        {
+          texto: 'Aprender Vue.js',
+          terminada: false
+        },
+        {
+          texto: 'Aprender React.js',
+          terminada: false
+        },
+        {
+          texto: 'Aprender React Native',
+          terminada: false
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+
+</style>
